e1ffdd6c0bd147c7cf4fb347b2383f338bdc9529
Anas Ashfaq <anasashfaq40@gmail.com>
2025-09-14 14:10:43 +0500

    store drawer update

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
+  await Firebase.initializeApp (
     options: DefaultFirebaseOptions.currentPlatform, // required on web
   );
 
